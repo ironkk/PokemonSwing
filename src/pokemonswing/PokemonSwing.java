52efb0c63d106c2a70cdb6df8369bc3d13223bd4
ironkk <xavi.manzano1@gmail.com>
2017-11-16 12:22:38 +0100

    Main y enlace a main

---
+++ src/pokemonswing/PokemonSwing.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemonswing;
+
+import modelo.Pokemon;
+import modelo.PokemonAgua;
+import modelo.PokemonFuego;
+import modelo.PokemonPlanta;
+
+/**
+ *
+ * @author DAM
+ */
+public class PokemonSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Los Pokemon deberán guardarse en un HashMap. No pueden existir dos Pokemon con el mismo nombre.
+        Principal ps = new Principal();
+        ps.setLocationRelativeTo(null);
+        ps.setVisible(true);
+
+    }
+
+}
